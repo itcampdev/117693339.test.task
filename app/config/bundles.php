9dca6657aab3b44181241e201a2eb46730456369
Vladimir Ivanov <ivanov.vladimir.v@yandex.ru>
2025-03-16 18:21:48 +0300

    добавил авторизацию

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
